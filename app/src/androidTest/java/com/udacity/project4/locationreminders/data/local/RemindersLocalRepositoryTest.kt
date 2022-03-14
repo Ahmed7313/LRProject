caa9c1a6b0e20c5487c9de358aa5956587ff9c4b
AHMED Rabee <35776251+Ahmed7313@users.noreply.github.com>
2022-03-14 23:23:09 +0200

    Working on RemindersLocalRepositoryTest.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -10,13 +10,14 @@ import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -25,6 +26,47 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
 
+    private lateinit var remindersLocalRepository: RemindersLocalRepository
+    private lateinit var database: RemindersDatabase
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    @Before
+    fun setUp(){
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java).allowMainThreadQueries().build()
+
+        remindersLocalRepository = RemindersLocalRepository(database.reminderDao(), Dispatchers.Main)
+    }
+
+
+    @After
+    fun cleanUp(){
+        database.close()
+    }
+
+    private fun getReminder(): ReminderDTO {
+        return ReminderDTO(
+            title = "Title",
+            description = "description",
+            location = "Cairo",
+            latitude = 1.1,
+            longitude = 2.2)
+    }
+
+    @Test
+    fun getReminders() = testScope.runBlockingTest {
+        val reminder = getReminder()
+        remindersLocalRepository.saveReminder(reminder)
+        val loadedReminder = (remindersLocalRepository.getReminder(reminder.id) as? Result.Success)?.data
+
+        Assert.assertThat<ReminderDTO>(loadedReminder as ReminderDTO, CoreMatchers.notNullValue())
+        Assert.assertThat(loadedReminder, `is`(reminder))
+    }
 }
